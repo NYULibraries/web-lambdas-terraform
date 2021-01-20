92e1e94fb961d67e23c62f16617d67954f09f76f
BAlter <barnaby.alter@nyu.edu>
2021-01-19 23:03:31 -0500

    Remove unused scripts

--- scripts/pull_quay.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -ex
-export PROJECT_NAME=web-lambdas-terraform
-docker pull quay.io/nyulibraries/${PROJECT_NAME}:${CIRCLE_BRANCH//\//_} || docker pull quay.io/nyulibraries/${PROJECT_NAME}:latest
--- scripts/push_quay.sh
+++ scripts/push_quay.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -ex
 export PROJECT_NAME=web-lambdas-terraform
-# docker tag ${PROJECT_NAME} quay.io/nyulibraries/${PROJECT_NAME}:${CIRCLE_BRANCH//\//_}
-# docker tag ${PROJECT_NAME} quay.io/nyulibraries/${PROJECT_NAME}:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
-# docker tag ${PROJECT_NAME} quay.io/nyulibraries/${PROJECT_NAME}:latest
 docker push quay.io/nyulibraries/${PROJECT_NAME}:${CIRCLE_BRANCH//\//_}
 docker push quay.io/nyulibraries/${PROJECT_NAME}:${CIRCLE_BRANCH//\//_}-${CIRCLE_SHA1}
 docker push quay.io/nyulibraries/${PROJECT_NAME}:latest
