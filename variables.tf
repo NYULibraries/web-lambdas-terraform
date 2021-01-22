338fbffe8adb7a7bb3dcd2dc0a986f1a2762ca8a
BAlter <barnaby.alter@nyu.edu>
2021-01-21 20:53:54 -0500

    Prettify spacing

--- variables.tf
+++ variables.tf
@@ -1,53 +1,53 @@
 # To be defined by the context
 variable "lambda_exec_arn" {
-	type = string
+  type = string
 }
 variable "apigw_id" {
-	type = string
+  type = string
 }
 variable "apigw_root_resource_id" {
-	type = string
+  type = string
 }
 variable "apigw_execution_arn" {
-	type = string
+  type = string
 }
 variable "lambda_s3_bucket" {
-	type = string
+  type = string
 }
 variable "aws_username" {
-	type = string
+  type = string
 }
 variable "apigw_stage" {
-	type = string
+  type = string
 }
 
 # To be defined per application
 variable "lambda_version" {
-	type = string
+  type = string
 }
 variable "lambda_function_name" {
-	type = string
+  type = string
 }
 variable "lambda_handler" {
-	type = string
+  type = string
 }
 variable "lambda_runtime" {
-	type = string
+  type = string
 }
 variable "lambda_method" {
-	type = string
+  type = string
 }
 variable "lambda_memory_size" {
-	type = number
+  type    = number
   default = 128
 }
 variable "environment_variables" {
-  type        = map
+  type        = map(any)
   description = "Environment variables for the lambda"
   default     = {}
 }
 variable "lambda_cw_schedule_expression" {
-	type = string
-	default = ""
+  type    = string
+  default = ""
 }
 
