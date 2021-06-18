2ae488b83a7f85de9caea08788e5bc11513aa205
Eric Griffis <eric.griffis@nyu.edu>
2021-06-18 14:24:47 -0400

    Use yq instead of sed for env setting

--- scripts/set_env.sh
+++ scripts/set_env.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-sed -nr '/'$1':/,$ s/  ([A-Za-z_]+): (.*)/export \1=\2/ p' deploy.yml | xargs -0 > .env
+yq -M eval '.'$1'' deploy.yml | xargs -0 > .env
 source .env
 
 export TF_VAR_environment_variables=$(envsubst < .tf_env_vars)
